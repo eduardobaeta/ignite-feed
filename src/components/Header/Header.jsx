697794d9661505d28a559e232eef2c7093a308f0
Eduardo Brumassio <eduardobrumassio@hotmail.com>
2022-06-26 12:57:36 -0300

    feature: add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import StyleHeader from './Header.module.css'
+
+export function Header(){
+    return(
+        <header className={StyleHeader.header}>
+            <h1>Ignite Feed</h1>
+        </header>
+    )
+}
