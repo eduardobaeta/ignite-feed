6bb874769c49da074723242897d35d509b9940b4
Eduardo Brumassio <eduardobrumassio@hotmail.com>
2022-06-26 13:57:13 -0300

    feature: update app and app.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import { Header } from './components/Header/Header'
 
+import StyleApp from './App.module.css'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <Header/>
-      <div className="wrapper">
+      <div className={StyleApp.wrapper}>
         <aside>
           sidebar
         </aside>
         <main>
-
+          post
         </main>
       </div>
     </div>
